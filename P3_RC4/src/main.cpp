c2563b54ff7442be7c2afa181e801c6a02458652
Bárbara <49285061+barbaragd@users.noreply.github.com>
2020-02-19 21:39:36 +0100

    P3: leer llave y msj por teclado mediante un string

--- P3_RC4/src/main.cpp
+++ P3_RC4/src/main.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
 #include "../include/RC4.hpp"
+#include <math.h> // para pow(base, exponente)
 
-int main() {
+std::vector<int> string2v_int(std::string msj)
+{
+    msj.push_back(',');
+    std::vector<int> mimsj;
+
+    std::vector<int> aux;
+    int r = 0;
+    int n = 0;
+    int exp = 0;
+    for (long unsigned int i = 0; i < msj.size(); i++)
+    {
+        if (msj[i] != ',')
+        {
+            n = msj[i] - 48;
+            aux.push_back(n); // si le resto 49, el 1 sería el 0
+        }
+        else
+        { // hay una coma, entonces es que ha leido el num
+            for (int j = aux.size() - 1; j >= 0; j--)
+            {
+                r += aux[j] * pow(10, exp);
+                exp++;
+            }
+            mimsj.push_back(r);
+            r = 0;
+            exp = 0;
+            aux.clear();
+            aux.resize(0);
+        }
+    }
+    return mimsj;
+}
+
+int main()
+{
     std::vector<int> millave;
     std::vector<int> mimsj;
 
-    millave.push_back(2);
-    millave.push_back(5);
+    std::string msj;
+    std::cout << "Intro msj: ";
+    std::cin >> msj;
+    mimsj = string2v_int(msj);
 
-    mimsj.push_back(1);
-    mimsj.push_back(34);
-
+    std::string key;
+    std::cout << "Intro key: ";
+    std::cin >> key;
+    millave = string2v_int(key);
+    
     RC4 micifrado(millave);
     micifrado.cifrar(mimsj);
 
-    std::vector<int> miotromsj;
-    miotromsj.push_back(145);
-    miotromsj.push_back(44);
-    micifrado.cifrar(miotromsj);
 }
